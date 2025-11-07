17c4012c38f774da95617dfbd8995f1bb36933ff
310623104067 <310623104067@eec.srmrmp.edu.in>
2025-11-07 13:03:07 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,11 @@
+let a=10;
+console.log(a);
+{
+let b=20;
+console.log(b);
+}
+const arr= [1,2,3,4,5,6,7,8,9,10];
+console.log(Math.floor(Math.random() *6));
+console.log(arr[Math.floor(Math.random() * arr.length)]);
+let ele= document.getElementById("div1");
+ele.innerHTML = "<h1>Hello World</h1>";
